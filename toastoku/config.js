b95ab4433dc66484929b9690e63ff9eb7b47568c
Noah <56979358+gonoahwhere@users.noreply.github.com>
2025-11-06 12:53:25 +0000

    Create config.js

---
+++ toastoku/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    botid: "1383844272989540422",
+    settings: {
+        prefix: "/",
+        ver: "1.0.0",
+        dob: "15th June 2025"
+    },
+    owner: {
+        tag: "gonoahwhere",
+        id: "372456601266683914",
+        mention: "<@!372456601266683914>"
+    },
+    embed: {
+        embedFooter: "✨ I made grid with shapes!",
+        embedColor: "#FFFFFF"
+    },
+    devs: {
+        noah: "<@!372456601266683914>",
+    }
+}
