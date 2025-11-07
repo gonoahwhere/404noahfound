e680a18ce2f6e89c2238717d5e0034ee761aaae7
Noah <56979358+gonoahwhere@users.noreply.github.com>
2025-11-07 16:49:18 +0000

    Create calendar_version_2.cpp

---
+++ calendar/calendar_version_2.cpp
@@ -0,0 +1,75 @@
+#include <string>
+#include <iostream>
+#include <iomanip> /* Provides formatting, useful for the calendar layout */
+#include <ctime> /* Provides ability to use today's date */
+
+using namespace std;
+
+bool leapYearChecker(int year) { return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0); } /* Creates a True/False statement to check if year given is divisible by 4 or 400, but not by 100 (centuries aren't leap years!) */
+
+int generateCorrectDay(int month, int year) {
+	tm time_in = {}; /* Creates empty variables, used to correctly find the start day of the month */
+	time_in.tm_year = year - 1900; /* Calculate how many years since 1900 */
+	time_in.tm_mon = month - 1; /* Starts at 0, so September would be 8 */
+	time_in.tm_mday = 1; /* First day of the month is always 1 */
+	mktime(&time_in); /* Calculates the correct day of the week */
+	return time_in.tm_wday; /* Returns the first weekday of the month, 1 would represent Monday */
+}
+
+void generateCalendar(int month, int year, int startDay, int highlightDay = 0) {
+	string monthNames[] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" }; /* Creates an array of the names for each month */
+	int daysInMonth[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; /* Creates an array of the number of days in each month */
+
+	if (leapYearChecker(year)) daysInMonth[1] = 29; /* Adds a 29th day to February for leap years */
+	cout << "       " << monthNames[month - 1] << "  " << year << "\n---------------------------\nSun Mon Tue Wed Thu Fri Sat\n"; /* Prints month, year and shorthand names for the week */
+
+	for (int i = 0; i < startDay; i++) { /* Prints spaces before the first day of the month */
+		cout << "    ";
+	}
+
+	for (int day = 1; day <= daysInMonth[month - 1]; day++) { /* Generates the days, starting at 1 in a 7 day format */
+		if (day == highlightDay) cout << "\033[33m" << setw(3) << day << "\033[0m "; /* Highlights the given day, or today if the user picked 'today' */
+		else cout << setw(3) << day << " "; /* No highlight on other days */
+		if ((day + startDay) % 7 == 0) cout << "\n";
+	}
+}
+
+bool validateChecker(int &day, int &month, int &year) { /* Checks if the user provides a valid month, year or day, if not allows a retry */
+	int daysInMonth[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	do { cout << "Enter a Month [1-12]: "; cin >> month; if (month < 1 || month > 12) cout << "Invalid month, please enter 1-12!\n"; } /* Prints a string to the console and requires user input */
+	while (month < 1 || month > 12); /* Lets user try again until valid value */
+	do { cout << "Enter a Year [1970+]: "; cin >> year; if (year < 1970) cout << "Invalid year, please enter a year from 1970 onwards!\n"; }
+	while (year < 1970);
+	if (leapYearChecker(year)) daysInMonth[1] = 29;
+	do { cout << "Enter a Day [1-" << daysInMonth[month - 1] << "]: "; cin >> day; if (day < 1 || day > daysInMonth[month - 1]) cout << "Invalid day, please enter 1-" << daysInMonth[month - 1] << "!\n"; }
+	while (day < 1 || day > daysInMonth[month - 1]); cout << "\n\n";
+	int startDay = generateCorrectDay(month, year);
+	generateCalendar(month, year, startDay, day);
+	cout << "\n\n"; return 0; /* Allows 'Press any key to continue... to be on a new line instead of next to the days */
+}
+
+bool todayDisplay(int& today, int& month, int& year) {
+	time_t t = time(nullptr); /* Grabs the current time, in seconds since Jan 1st 1970 */
+	tm now; /* holds the variables for local time */
+	localtime_s(&now, &t); /* Converts the time in seconds so its readable, into yy/mm/dd /h/m/s */
+	today = now.tm_mday; /* Calculates which date is today */
+	month = now.tm_mon + 1; /* Allows month to start at 1 instead of 0, for 1 - 12 format */
+	year = now.tm_year + 1900; /* Calculates how many years it has been since 1900, for accuracy */
+	int startDay = generateCorrectDay(month, year); /* Function to prevent repeated code, generates correct start day of the month */
+	generateCalendar(month, year, startDay, today); /* Function to prevent repeated code, generates calendar for the given month/year */
+	cout << "\n\n"; system("pause"); return 0;
+}
+
+int main()
+{
+	string choice;
+	int daysInMonth[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	int today, month, year;
+	cout << "Type 'today' to use today's date, or 'let me pick' to choose your own!\n"; /* Gives the user two choices */
+	getline(cin, choice); /* Accepts string of text, rather than 1 word arguments */
+	cout << "\n\n";
+	int chosenDay;
+
+	if (choice == "today") { todayDisplay(today, month, year); } /* Grabs today's date to display */
+	if (choice == "let me pick") { validateChecker(chosenDay, month, year); } /* Sets the values user has chosen */
+}
